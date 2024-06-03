e9b22e8f3cbe8fdce9a2fa724dd5b477af7d9e84
Saxhaa <sacha.bonicatto@gmail.com>
2024-06-03 22:35:36 +0200

    test ultime

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! ca va?" );
+        System.out.println( "Hello World! ca va? oui merci" );
     }
 }
 //test
